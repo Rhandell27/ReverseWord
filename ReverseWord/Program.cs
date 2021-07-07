9457ff286345f2cff844acfd4e3f46215f72f4c3
Snow In Summer <Snow In Summer@DESKTOP-R3EIT0U>
2021-07-07 17:02:05 +0800

    Add project files.

---
+++ ReverseWord/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReverseWord
+{
+    class Word
+    {
+        string newWord = "";
+        public string ReverseWord(string words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                var n = words.Length - 1;
+                var temp = n - i;
+                newWord += words[temp];
+                
+            }
+            return newWord;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = new Word();
+            Console.WriteLine("Type a word: ");
+            var newWord = Console.ReadLine();
+            Console.WriteLine("Your new Reverse Word: \"{0}\"", word.ReverseWord(newWord));
+            Console.Read();
+
+        }
+    }
+}
